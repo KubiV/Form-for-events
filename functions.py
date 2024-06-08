ab5bc8c0c7f20a7294e85ad333d54b3264cf0e4d
Jakub Vávra <56688914+KubiV@users.noreply.github.com>
2024-06-08 10:53:14 +0200

    updated code generating

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import csv
 import random
 import string
+import unicodedata
 
 # Function for checking registrations
 def get_registrations_count_csv():
@@ -14,13 +15,21 @@ def get_registrations_count_csv():
 
 # Generates unique code for unregistering from the event
 def generate_unique_code(first, second):
-    # Get 3 first letters of the first and secornd part for the code
-    code_prefix = (first[:3] if len(first) >= 3 else first) + (second[:3] if len(second) >= 3 else second)
+    # Replace special characters in first and second strings
+    first_clean = ''.join(replace_special_char(char) for char in first)
+    second_clean = ''.join(replace_special_char(char) for char in second)
+    
+    # Get 3 first letters of the cleaned first and second part for the code
+    code_prefix = (first_clean[:3] if len(first_clean) >= 3 else first_clean) + (second_clean[:3] if len(second_clean) >= 3 else second_clean)
     # Code suffix
     code_suffix = ''.join(random.choices(string.digits, k=3))
 
     unique_code = code_prefix + code_suffix
     return unique_code
+
+def replace_special_char(char):
+    # Replace special characters with their regular counterparts
+    return unicodedata.normalize('NFD', char).encode('ascii', 'ignore').decode('utf-8')
 
 # Extract id
 def extract_id(file_txt):
@@ -28,3 +37,4 @@ def extract_id(file_txt):
         # Read the first line
         id = file.readline().strip()
         return id
+    
